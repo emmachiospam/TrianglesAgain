329096b4afdc10b539c31238b690800241de11dc
emmachio <emmany2409@gmail.com>
2020-11-01 00:52:23 -0400

    tested perimeter

--- Tester.java
+++ Tester.java
@@ -1,10 +1,12 @@
 public class Tester {
 
   public static void main(String[] args) {
-    Point p1 = new Point(3,2 );
-    Point p2 = new Point(3,4);
+    // Point p1 = new Point(3,2 );
+    // Point p2 = new Point(3,4);
     // System.out.println(p1.distanceTo(p2));
-    System.out.println(p1.equals(p2));
+    // System.out.println(p1.equals(p2));
+    Triangle t1 = new Triangle(0,0,4,0,4,3);
+    System.out.println(t1.getPerimeter());
   }
 
 }
