df12058a4ed83d97ab79fda81b72ac4378a52804
emmachio <emmany2409@gmail.com>
2020-11-01 00:51:02 -0400

    added perimeter

--- Triangle.java
+++ Triangle.java
@@ -8,13 +8,13 @@ public class Triangle{
   }
 
   public Triangle(double x1, double y1,double x2, double y2,double x3, double y3) {
-    Point v1 = new Point (x1, y1);
-    Point v2 = new Point (x2, y2);
-    Point v3 = new Point (x3, y3);
+    v1 = new Point(x1, y1);
+    v2 = new Point(x2, y2);
+    v3 = new Point(x3, y3);
   }
 
   public double getPerimeter() {
-    return(v1.distanceTo(v2) + v2.distanceTo(v3) + v3.distanceTo(v1));
+    return (v1.distanceTo(v2) + v2.distanceTo(v3) + v3.distanceTo(v1));
   }
 
 }
