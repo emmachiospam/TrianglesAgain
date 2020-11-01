809851263615cbfd8036dc393f961531d8afb715
emmachio <emmany2409@gmail.com>
2020-11-01 00:12:05 -0400

    done

---
+++ Tester.java
@@ -0,0 +1,10 @@
+public class Tester {
+
+  public static void main(String[] args) {
+    Point p1 = new Point(3,2 );
+    Point p2 = new Point(3,4);
+    // System.out.println(p1.distanceTo(p2));
+    System.out.println(p1.equals(p2));
+  }
+
+}
