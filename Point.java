aef14a3dc7d27890d6daa1c23dc7863fb6cd7422
emmachio <emmany2409@gmail.com>
2020-11-01 00:25:38 -0400

    equals updated

--- Point.java
+++ Point.java
@@ -3,14 +3,14 @@ public class Point{
 
   //construct a point given coordinates
   public Point(double X, double Y){
-    x=X;
-    y=Y;
+    x = X;
+    y = Y;
   }
 
   //construct a point given a point  (redundant , just provides flexibility)
   public Point(Point p){
-    x= p.x;  //private access in the same file is allowed!
-    y= p.y;
+    x = p.x;  //private access in the same file is allowed!
+    y = p.y;
   }
 
   public double getX(){
@@ -19,6 +19,15 @@ public class Point{
 
   public double getY(){
     return y;
+  }
+
+  public double distanceTo(Point other) {
+    double result = Math.sqrt(((other.x - this.x) * (other.x - this.x)) + ((other.y - this.y) * (other.y - this.y)));
+    return result;
+  }
+
+  public boolean equals(Point other) {
+    return(this.x == other.x && this.y == other.y);
   }
 
 }
