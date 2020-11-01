584b8a3a13609a86f42e9e988a1138893caafd5b
emmachio <emmany2409@gmail.com>
2020-11-01 01:37:59 -0400

    added toString, have not tried replacement

--- Triangle.java
+++ Triangle.java
@@ -33,4 +33,21 @@ public class Triangle{
       return "it is a scalene";
     }
   }
+
+  public String toString() {
+    return("v1: (" + v1.getX() + ", " + v1.getY() + ") v2: (" + v2.getX() + ", " + v2.getY() + ") v3: (" + v3.getX() + ", " + v3.getY() + ")");
+  }
+
+  public void setVertex(int index, Point newP) {
+    if(index == 0) {
+      v1 = newP;
+    }
+    else if(index == 1) {
+      v2 = newP;
+    }
+    else if(index == 2) {
+      v3 = newP;
+    }
+  }
+
 }
